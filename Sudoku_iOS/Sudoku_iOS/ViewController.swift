8a17830825600ad9fe77f9c5ec681fdfe0c50edd
Tongfei Li <tongfeili@caidaomimajiugeiniyong.local>
2020-02-11 20:19:57 -0500

    xcode

---
+++ Sudoku_iOS/Sudoku_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sudoku_iOS
+//
+//  Created by Tongfei Li on 2020/2/11.
+//  Copyright © 2020年 Tongfei Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
